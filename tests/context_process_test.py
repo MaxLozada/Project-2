43ce85102e9a992cfe0df19d05ebae1b4ce93b1b
MaxLozada <56085654+MaxLozada@users.noreply.github.com>
2022-03-31 23:34:00 -0400

    Update context_process_test.py

--- tests/context_process_test.py
+++ tests/context_process_test.py
@@ -2,8 +2,6 @@
 import datetime
 from os import getenv
 
-# pylint: disable=f-string-without-interpolation
-
 def test_context_variables_environment(client):
     """This test checks if the environment is printed"""
     response = client.get("/")
